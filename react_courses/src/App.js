ad70d8a599a9e3a60dd76cc10172e454bd64f80c
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-11-02 22:32:09 +0300

    add handleclick on buttons

--- react_courses/src/App.js
+++ react_courses/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 
 import { AppHeader } from './components/AppHeader/AppHeader';
@@ -9,26 +10,32 @@ function App() {
   const todoData = [
     {
       label: 'first line',
-      important: false
+      important: false,
+      active: true
     },
     {
       label: 'second line',
-      important: true
+      important: true,
+      active: true
     },
     {
       label: 'third line',
-      important: false
+      important: false,
+      active: false
     },
   ]
-
+  const [todoDataList, setTodoData] = useState(todoData)
+  const filterAll = () => setTodoData(todoData)
+  const filterActive = () => setTodoData(todoData.filter(el => el.active))
+  const filterDone = () => setTodoData(todoData.filter(el => !el.active))
   return (
     <div className="App">
       <AppHeader toDo={1} done={3}/>
       <div className='top-panel d-flex'>
       <SearchPanel />
-      <ItemStatusBar />
+      <ItemStatusBar filterAll={filterAll} filterActive={filterActive} filterDone={filterDone}/>
       </div>
-      <TodoList todoData={todoData} />
+      <TodoList todoData={todoDataList} />
     </div>
   );
 }
--- react_courses/src/components/ItemStatusBar/ItemStatusBar.js
+++ react_courses/src/components/ItemStatusBar/ItemStatusBar.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import './ItemStatusBar.css';
 
-export const ItemStatusBar = () => {
+export const ItemStatusBar = ({ filterAll, filterActive, filterDone }) => {
   return (
     <div className='btn-group'>
-      <button type='button' className='btn btn-info'>All</button>
-      <button type='button' className='btn btn-outline-secondary'>Active</button>
-      <button type='button' className='btn btn-outline-secondary'>Done</button>
+      <button type='button' className='btn btn-info' onClick={() => filterAll()}>All</button>
+      <button type='button' className='btn btn-outline-secondary' onClick={() => filterActive()}>Active</button>
+      <button type='button' className='btn btn-outline-secondary' onClick={() => filterDone()}>Done</button>
     </div>
   )
 }
