0592820c24bc316e3bb517fefdbb39dfd81223f0
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-11-02 23:02:39 +0300

    buttons fix

--- react_courses/src/components/ItemStatusBar/ItemStatusBar.js
+++ react_courses/src/components/ItemStatusBar/ItemStatusBar.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
 
 import './ItemStatusBar.css';
 
 export const ItemStatusBar = ({ filterAll, filterActive, filterDone }) => {
+  const buttons = ['All', 'Active', 'Done']
+  const [active, setActive] = useState([true, false, false])
+  const activeHandle = (item) => {
+    switch(item) {
+      case 0: {
+        setActive([true, false, false])
+        filterAll()
+        break
+      }
+      case 1: {
+        setActive([false, true, false])
+        filterActive()
+        break
+      }
+      case 2: {
+        setActive([false, false, true])
+        filterDone()
+        break
+      }
+      default: break
+    }
+  }
   return (
     <div className='btn-group'>
-      <button type='button' className='btn btn-info' onClick={() => filterAll()}>All</button>
-      <button type='button' className='btn btn-outline-secondary' onClick={() => filterActive()}>Active</button>
-      <button type='button' className='btn btn-outline-secondary' onClick={() => filterDone()}>Done</button>
+      {
+        buttons.map((el, i) => {
+          return <button type='button' className={classnames('btn', { 'btn-info': active[i], 'btn-outline-secondary': !active[i] })}
+            onClick={() => activeHandle(i)} key={i}>{el}</button>
+        })
+      }
     </div>
   )
 }
