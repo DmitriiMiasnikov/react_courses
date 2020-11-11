3a9622425e0fb99e6ef0a9c149e4225633bdb3be
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-11-11 23:03:23 +0300

    fix todoitems buttons with long text

--- todo/src/components/TodoList/TodoListItem/TodoListItem.js
+++ todo/src/components/TodoList/TodoListItem/TodoListItem.js
@@ -17,10 +17,7 @@ export const TodoListItem = ({ label, important, removeTodo, id }) => {
     fontWeight: importantItem ? 'bold' : 'normal'
   }
   return (
-    <span className={classnames('todo-list-item', { 'done': done })}>
-      <span className={classnames('todo-list-item-label')} style={styles} onClick={() => setDoneHandler()}>
-        {label}
-      </span>
+    <div className={classnames('todo-list-item', { 'done': done })}>
       <button type='button' className='btn btn-outline-success btn-sm float-right' onClick={() => setImportantItemHandler()}>
         <svg width='20' height='20' viewBox="0 0 16 16" className="bi bi-exclamation" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
           <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
@@ -32,7 +29,10 @@ export const TodoListItem = ({ label, important, removeTodo, id }) => {
           <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
         </svg>
       </button>
-    </span>
+      <span className={classnames('todo-list-item-label')} style={styles} onClick={() => setDoneHandler()}>
+        {label}
+      </span>
+    </div>
 
   )
 }
