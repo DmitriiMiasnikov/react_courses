a9ad40db1486e487433e1997c66899409853dbb3
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-11-13 02:33:20 +0300

    refactoring

--- stars/src/components/RandomPlanet/RandomPlanet.js
+++ stars/src/components/RandomPlanet/RandomPlanet.js
@@ -16,34 +16,35 @@ export const RandomPlanet = () => {
   useEffect(() => {
     updatePlanet()
   }, [])
-  if (loading) {
-    return (
-      <div className='random-planet jumptron rounded loading'>
-      <img src={loadingImg}></img>
-      </div>
-    )
-  } else return (
-    <div className='random-planet jumptron rounded'>
-      <img className='planet-image' src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
-      <div>
-        <h4>
-          {planet.name}
-        </h4>
-        <ul className='list-group list-group-flush'>
-          <li className='list-group-item'>
-            <span className='term'>Poulation</span>
-            {planet.population}
-          </li>
-          <li className='list-group-item'>
-            <span className='term'>Rotation Period</span>
-            {planet.rotationPeriod}
-          </li>
-          <li className='list-group-item'>
-            <span className='term'>Diameter</span>
-            {planet.diameter}
-          </li>
-        </ul>
-      </div>
+  return (
+    <div className={loading ? 'random-planet jumptron rounded loading' : 'random-planet jumptron rounded'}>
+      {
+        loading ? <img src={loadingImg}></img> : (
+          <>
+            <img className='planet-image' src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
+            <div>
+              <h4>
+                {planet.name}
+              </h4>
+              <ul className='list-group list-group-flush'>
+                <li className='list-group-item'>
+                  <span className='term'>Poulation</span>
+                  {planet.population}
+                </li>
+                <li className='list-group-item'>
+                  <span className='term'>Rotation Period</span>
+                  {planet.rotationPeriod}
+                </li>
+                <li className='list-group-item'>
+                  <span className='term'>Diameter</span>
+                  {planet.diameter}
+                </li>
+              </ul>
+            </div>
+          </>
+        )
+      }
+
     </div>
   )
 }
