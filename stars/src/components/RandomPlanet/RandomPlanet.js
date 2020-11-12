544365b942d2e982a1bbaf6631e55e02ab960aa7
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-11-12 23:47:10 +0300

    add loading

--- stars/src/components/RandomPlanet/RandomPlanet.js
+++ stars/src/components/RandomPlanet/RandomPlanet.js
@@ -1,19 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import './RandomPlanet.css';
 import { swapi } from './../../Servises/swapiServises'
+import loadingImg from './../../assets/loading.svg'
 
 export const RandomPlanet = () => {
   const [planet, setPlanet] = useState([])
-  const id = Math.floor(Math.random()*20) + 2;
+  const [loading, setLoading] = useState(true);
+  const id = Math.floor(Math.random() * 20) + 2;
   const updatePlanet = () => {
     swapi.getPlanet(id).then((res) => {
       setPlanet(res)
+      setLoading(false)
     })
   }
   useEffect(() => {
     updatePlanet()
   }, [])
-  return (
+  if (loading) {
+    return (
+      <div className='random-planet jumptron rounded loading'>
+      <img src={loadingImg}></img>
+      </div>
+    )
+  } else return (
     <div className='random-planet jumptron rounded'>
       <img className='planet-image' src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
       <div>
@@ -22,15 +31,15 @@ export const RandomPlanet = () => {
         </h4>
         <ul className='list-group list-group-flush'>
           <li className='list-group-item'>
-            <span className='term'>Poulation</span> 
+            <span className='term'>Poulation</span>
             {planet.population}
           </li>
           <li className='list-group-item'>
-            <span className='term'>Rotation Period</span> 
+            <span className='term'>Rotation Period</span>
             {planet.rotationPeriod}
           </li>
           <li className='list-group-item'>
-            <span className='term'>Diameter</span> 
+            <span className='term'>Diameter</span>
             {planet.diameter}
           </li>
         </ul>
