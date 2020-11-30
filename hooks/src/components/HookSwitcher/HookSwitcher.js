3dbd869ef2763912c00ee20805d92a67bd103b74
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-11-30 03:22:25 +0300

    switcher forn size

--- hooks/src/components/HookSwitcher/HookSwitcher.js
+++ hooks/src/components/HookSwitcher/HookSwitcher.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
+import './HookSwitcher.css'
 
 export const HookSwitcher = ({}) => {
-  const [color, setColor] = useState('grey')
+  const [color, setColor] = useState('grey');
+  const [fontSize, setFontSize] = useState(14);
   const greySwitch = () => setColor('grey');
   const blackSwitch = () => setColor('black');
   return (
-    <div style={{ padding:'20px', backgroundColor: color, width: '200px' }}>
+    <div className='wrapper' 
+      style={{ backgroundColor: color, fontSize: `${fontSize}px` }}>
+      <span className='text'>just a text</span>
       <button onClick={() => greySwitch()}>grey</button>
       <button onClick={() => blackSwitch()}>black</button>
+      <button onClick={() => setFontSize((s) => s + 2)}>+</button>
+      <button onClick={() => setFontSize((s) => s - 2)}>-</button>
     </div>
   )
 }
