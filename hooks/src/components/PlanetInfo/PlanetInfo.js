4067f20e4897e9dd16aaf2535106331cb36ea6dd
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2020-12-19 18:17:24 +0300

    fix

--- hooks/src/components/PlanetInfo/PlanetInfo.js
+++ hooks/src/components/PlanetInfo/PlanetInfo.js
@@ -1,17 +1,33 @@
-import React, {useState, useEffect, useCallback} from 'react';
-
+import React, { useState, useEffect, useCallback } from 'react';
 
 const getPlanet = (id) => {
   return fetch(`https://swapi.dev/api/planets/${id}`)
-  .then(res => res.json())
-  .then(data => data)
+    .then(res => res.json())
+    .then(data => data)
 }
 
 const useRequest = (request) => {
-  const [dataState, setDataState] = useState(null);
+  const [dataState, setDataState] = useState({
+    data: null,
+    loading: true,
+    error: null,
+  });
   useEffect(() => {
+    setDataState({
+      data: null,
+      loading: true,
+      error: null,
+    })
     let cancelled = false;
-    request().then(data => !cancelled && setDataState(data))
+    request().then(data => !cancelled && setDataState({
+      data,
+      loading: false,
+      error: null
+    })).catch(error => !cancelled && setDataState({
+      data: null,
+      loading: false,
+      error
+    }))
     return () => cancelled = true;
   }, [request])
   return dataState;
@@ -19,19 +35,21 @@ const useRequest = (request) => {
 
 const usePlanetInfo = (id) => {
   const request = useCallback(() => getPlanet(id), [id])
-  return useRequest(request)
+  return useRequest(request);
 }
 
 export const PlanetInfo = ({ id }) => {
   const [planetId, setPlanetId] = useState(id);
-  const data = usePlanetInfo(planetId);
+  const {data, loading, error} = usePlanetInfo(planetId);
   return (
     <div>
       <div>
         <button onClick={() => setPlanetId((id) => id + 1)}>+</button>
         <button onClick={() => setPlanetId((id) => id - 1)}>-</button>
       </div>
-      {data ? `${planetId} - ${data.name}` : `${planetId} - no planet`} - planet name
+        {error && <div>{planetId} - no planet</div>}
+        {loading && <div>...loading</div>}
+        {data && <div>{planetId} - {data.name}</div>}
     </div>
   )
 }
