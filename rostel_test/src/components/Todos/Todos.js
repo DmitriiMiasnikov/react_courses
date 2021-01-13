1fe2bc93df97329bc20043383c1d9e0fb564304a
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2021-01-14 02:24:07 +0300

    add sort button

--- rostel_test/src/components/Todos/Todos.js
+++ rostel_test/src/components/Todos/Todos.js
@@ -6,6 +6,7 @@ import { Loading } from '../Loading/Loading';
 export const Todos = ({ getTodos, getUserInfo }) => {
   const [todoList, setTodoList] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [sortedByCompleted, setSortedByCompleted] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       const todos = await getTodos()
@@ -14,8 +15,24 @@ export const Todos = ({ getTodos, getUserInfo }) => {
     }
     fetchData()
   }, [getTodos])
+  const sortByCompleted = async () => {
+    setLoading(true);
+    const todos = await getTodos()
+    setLoading(false);
+    setTodoList(todos.sort((a, b) => {
+      if (sortedByCompleted) {
+        return (a.completed === b.completed)? 0 : a.completed? -1 : 1;
+      } else return (a.completed === b.completed)? 0 : a.completed? 1 : -1;
+    }));
+    setSortedByCompleted((item) => !item)
+  }
   return (
     <div className='todos'>
+      <div className='sort-buttons-block'>
+        <div className='sort-button' onClick={() => sortByCompleted()}>
+          <span>completed</span> {!sortedByCompleted ? <span>&#9660;</span> : <span>&#9650;</span>}
+        </div>
+      </div>
       {
         !loading ? todoList.map((el, i) => {
           return (
