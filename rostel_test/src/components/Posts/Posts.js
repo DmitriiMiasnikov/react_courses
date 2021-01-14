9bcf0268a028619579e26cc9000f7fdc9b3803f9
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2021-01-14 18:18:42 +0300

    fix

--- rostel_test/src/components/Posts/Posts.js
+++ rostel_test/src/components/Posts/Posts.js
@@ -7,7 +7,6 @@ export const Posts = ({ getPosts, setCurrentUser, getUsers }) => {
   const [postList, setPostList] = useState([]);
   const [loading, setLoading] = useState(true);
   const [usersList, setUsersList] = useState([]);
-  // const [sortedByCompleted, setSortedByCompleted] = useState(false);
   useEffect(() => {
     const fetchData = async () => {
       const posts = await getPosts();
@@ -18,24 +17,8 @@ export const Posts = ({ getPosts, setCurrentUser, getUsers }) => {
     }
     fetchData()
   }, [getPosts, getUsers])
-  // const sortByCompleted = async () => {
-  //   setLoading(true);
-  //   const todos = await getPosts()
-  //   setLoading(false);
-  //   setTodoList(todos.sort((a, b) => {
-  //     if (sortedByCompleted) {
-  //       return (a.completed === b.completed)? 0 : a.completed? -1 : 1;
-  //     } else return (a.completed === b.completed)? 0 : a.completed? 1 : -1;
-  //   }));
-  //   setSortedByCompleted((item) => !item)
-  // }
   return (
     <div className='posts'>
-      <div className='sort-buttons-block'>
-        {/* <div className='sort-button' onClick={() => sortByCompleted()}>
-          <span>completed</span> {!sortedByCompleted ? <span>&#9660;</span> : <span>&#9650;</span>}
-        </div> */}
-      </div>
       {
         !loading ? postList.map((el, i) => {
           return (
